1a04e96c38dd21ebdcc0f8e10213915ade52787e
Olesya Miller <olesyam.miller@gmail.com>
2019-12-20 05:09:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+class Anagram
+
+  def initialize(word)
+    @word = word 
+  end
+
+  def match(array) 
+    array.find_all do |word|
+     word.split("").sort.join == @word.split("").sort.join 
+    end
+  end
+  
+end
+
