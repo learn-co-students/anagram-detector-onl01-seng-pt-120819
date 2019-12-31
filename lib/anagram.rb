c3f868e1a7adcf8c3442a48db8f5e500efabb4eb
Andrei Pestenariu <pclandrei@gmail.com>
2019-12-31 03:49:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  
+  def match(array) 
+    array.find_all {|e| e.split('').sort == word.split('').sort}
+  end
+  
+end 
