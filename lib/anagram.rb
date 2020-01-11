91b407a8ccc3c643af91b4d511a78a963c788cd2
cedric e goddard <8ceg6@comcast.net>
2020-01-11 01:40:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :any_word 
+  
+  def initialize(any_word)
+      @any_word= any_word
+  end 
+  
+  def match(words)
+    words.find_all do |word|
+      if word.split("").sort == self.any_word.split("").sort 
+        word 
+        end
+        
+      end
+  end 
+end 
