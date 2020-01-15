77d899b6715baac5e65ee854f505e50b5ad7954b
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-01-15 18:51:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+ def match(array)
+   array.select do | letters |
+     if letters.split("").sort == @word.split("").sort
+       letters
+     end 
+   end 
+ end 
+  
+  
+end 
