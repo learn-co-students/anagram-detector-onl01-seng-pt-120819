d9ab89f089ab5d63c12aee379db4145f82d11a13
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-20 00:13:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    word_list.select do |letters|
+      @word.split("").sort == letters.split("").sort
+    end
+  end
+
+end
