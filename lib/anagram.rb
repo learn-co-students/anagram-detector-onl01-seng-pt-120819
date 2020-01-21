59b2af49b7dc6358fcb0db2e7b857c9fa7bca102
S. Makai Takori <makait87@gmail.com>
2020-01-21 18:27:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end  
+  
+  def match(words)
+    # iterate over the array of words that the .match method takes as an argument.
+    words.select do |word|
+      
+    # compare each word of that array to the word that the Anagram class is initialized with.
+    @word.split("").sort == word.split("").sort
+          
+    end  
+  end
+end  
