99906121ee6b0be11e91f972fea4a4122667492c
lavikoder <kevkamanda@gmail.com>
2020-02-08 13:49:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    wrd = self.word.split("").sort
+    #select alows you to only take what is relevant
+    array.select {|w| wrd == w.split("").sort}
+  end
+end
